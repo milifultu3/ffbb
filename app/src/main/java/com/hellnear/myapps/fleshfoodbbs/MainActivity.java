888ed8f95b3d5bd69d5c58523232f6626d7cbc00
sharedpc2 <mmilifultu@yahoo.co.jp>
2018-01-02 00:22:23 +0900

    test use map

--- app/src/main/java/com/hellnear/myapps/fleshfoodbbs/MainActivity.java
+++ app/src/main/java/com/hellnear/myapps/fleshfoodbbs/MainActivity.java
@@ -1,13 +1,23 @@
 package com.hellnear.myapps.fleshfoodbbs;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.MapFragment;
 
+public class MainActivity extends AppCompatActivity {
+    MapFragment mf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mf = MapFragment.newInstance();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(android.R.id.content, mf);
+        ft.commit();
+        //setContentView(R.layout.activity_main);
     }
 }
